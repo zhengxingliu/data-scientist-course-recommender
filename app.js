a5b6271b8bae993f43158b7111bb57eab98c15a6
zhengxingliu <45699390+zhengxingliu@users.noreply.github.com>
2020-11-17 00:49:50 -0500

    update view

--- app.js
+++ app.js
@@ -23,9 +23,7 @@ app.use((req, res, next) => {
 app.use(indeedRoutes)
 
 app.get('/',(req, res, next) => {
-  res.render('index', 
-  // { clickHandler:readSearchInput() }
-  )
+  res.render('index')
 })
 
 app.listen(process.env.PORT || 8080)
